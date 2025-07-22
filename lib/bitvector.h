6ac9aef5dde59484ccaadc3098d47b1ee8fbaac9
Arjang Talattof <talattof@gmail.com>
2025-07-22 01:21:35 -0700

    fixed bloom filter and working version of HLL

---
+++ lib/bitvector.h
@@ -0,0 +1,153 @@
+#ifndef BITVECTOR_H
+#define BITVECTOR_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+To implement a Bloom filter we need an auxilary data
+structure which helps us manipulate bits. This data structure
+compactly stores bits.
+
+https://en.wikipedia.org/wiki/Bit_array
+**/
+
+#define BITS_PER_BYTE (sizeof(unsigned int) * 8)
+#define BITS_IN_TYPE(type) (BITS_PER_BYTE * (sizeof(type)))
+
+typedef struct {
+	uint64_t *data;
+	size_t size;        // Number of elements
+} BitVector;
+
+BitVector *createBitVector(size_t num_bits) {
+	BitVector *bitv = malloc(sizeof(BitVector));
+	if (bitv == NULL) {
+		return NULL;
+	}
+
+	size_t mem_size = num_bits / BITS_IN_TYPE(uint64_t);
+    if (!(num_bits%BITS_IN_TYPE(u_int64_t))) {
+        mem_size++;
+    }
+	bitv->data = calloc(mem_size, sizeof(*(bitv->data)));
+    if (NULL==bitv->data) {
+        fprintf(stderr, "Out of memory.\n");
+        exit(EXIT_FAILURE);
+    }
+    bitv->size = num_bits;
+    return bitv;
+}
+
+void freeBitVector(BitVector *bitv) {
+    free(bitv->data);
+    free(bitv);
+}
+
+void setBit(BitVector *bitv, size_t index) {
+	if (index >= bitv->size) {
+        fprintf(stderr, "Out of bounds bit_idx=%zu, vect->size=%zu\n",
+                            index, bitv->size);
+        exit(EXIT_FAILURE);
+	}
+    size_t chunk_offset = index /  BITS_IN_TYPE(uint64_t);
+    size_t bit_offset = index & (BITS_IN_TYPE(uint64_t)-1);
+    uint64_t *byte = &(bitv->data[chunk_offset]);
+	*byte |= ((uint64_t)1) << bit_offset;
+}
+
+void clearBit(BitVector *bitv, size_t index) {
+	if (index >= bitv->size) {
+        fprintf(stderr, "Out of bounds bit_idx=%zu, vect->size=%zu\n",
+                            index, bitv->size);
+        exit(EXIT_FAILURE);
+	}
+    size_t chunk_offset = index /  BITS_IN_TYPE(uint64_t);
+    size_t bit_offset = index & (BITS_IN_TYPE(uint64_t)-1);
+    uint64_t *byte = &(bitv->data[chunk_offset]);
+	*byte &= ~(1 << bit_offset);
+}
+
+
+void flipBit(BitVector *bitv, size_t index) {
+	if (index >= bitv->size) {
+        fprintf(stderr, "Out of bounds bit_idx=%zu, vect->size=%zu\n",
+                            index, bitv->size);
+        exit(EXIT_FAILURE);
+	}
+    size_t chunk_offset = index /  BITS_IN_TYPE(uint64_t);
+    size_t bit_offset = index & (BITS_IN_TYPE(uint64_t)-1);
+    uint64_t *byte = &(bitv->data[chunk_offset]);
+	*byte &= *byte ^= ((uint64_t)1) << bit_offset;
+}
+
+int getBit(BitVector *bitv, size_t index) {
+	if (index >= bitv->size) {
+        fprintf(stderr, "Out of bounds bit_idx=%zu, vect->size=%zu\n",
+                            index, bitv->size);
+		return -1;
+	}
+    size_t chunk_offset = index / BITS_IN_TYPE(uint64_t);
+    size_t bit_offset = index & (BITS_IN_TYPE(uint64_t)-1);
+    uint64_t byte = bitv->data[chunk_offset];
+    return (byte>>bit_offset) & 1;
+}
+
+void printBits(BitVector *bitv, size_t size) {
+	if (bitv->data == NULL) {
+		return;
+	}
+    for (size_t i = 0; i < size; i++) {
+        printf("%d", getBit(bitv, i));
+    }
+	printf("\n");
+}
+
+void uint64_to_binary(uint64_t input, BitVector *bitv) {
+	printf("%llu\n", (uint64_t)8*sizeof(uint64_t));
+    if (bitv->size < 8*sizeof(uint64_t)) {
+        fprintf(stderr, "BitVector size too small for %llu\n", input);
+        exit(EXIT_FAILURE);
+    }
+    for (int i = 63; i >= 0; --i) {
+		unsigned int bit = input >> i & 1;
+		if (bit) {
+			setBit(bitv, 63-i);
+			printBits(bitv, 8*sizeof(uint64_t));
+			for (int c = 63; c > i; c--) {
+				printf("_");
+			}
+			printf("^\n");
+		}
+    }
+}
+
+void printBinary(uint64_t num, size_t len) {
+    for (int i = len-1; i >= 0; i--) {
+		uint64_t mask = (uint64_t)1 << i;
+		if (num & mask) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+    }
+    printf("\n");
+}
+
+int msb_position(uint64_t num, size_t offset) {
+	if (num == 0) {
+		return offset +1; // All bits are zeros
+	}
+
+	// Count leading zeros and add 1
+	size_t leading_zeros = 0;
+	uint64_t mask = 1UL << (offset - 1);
+	while (leading_zeros < offset && !(num & mask)) {
+		leading_zeros++;
+		mask >>= 1;
+	}
+	return leading_zeros + 1;
+}
+
+#endif
---
+++ lib/hash.h
@@ -0,0 +1,125 @@
+#ifndef HASHF_H
+#define HASHF_H
+
+#define DJB2_INIT 5381
+#define HASH64_NUM_FUNCTIONS 2
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef uint64_t (*hash64_func)(const void *data, size_t length);
+
+uint64_t djb2(const void *buff, size_t length) {
+    uint64_t hash = DJB2_INIT;
+    const uint8_t *data = buff;
+    for(size_t i = 0; i < length; i++) {
+         hash = ((hash << 5) + hash) + data[i];
+    }
+    return hash;
+}
+
+uint64_t sdbm(const void *buff, size_t length) {
+	uint64_t hash = 0;
+    const uint8_t *data = buff;
+    for(size_t i = 0; i < length; i++) {
+        hash = data[i] + (hash << 6) + (hash << 16) - hash;
+    }
+    return hash;
+}
+
+
+uint64_t hash_64(const void *buff, size_t len) {
+	uint64_t seed = 14695981039346656037ULL;
+	uint64_t prime = 1099511628211ULL;
+    const uint8_t *data = buff;
+    uint64_t h = seed;
+    for (size_t i = 0; i < len; ++i) {
+        h ^= data[i];
+        h *= prime;
+    }
+    return h;
+}
+
+uint64_t fnv_64(void *buf, size_t len, uint64_t hval) {
+    unsigned char *bp = (unsigned char *)buf;   /* start of buffer */
+    unsigned char *be = bp + len;               /* beyond end of buffer */
+
+    /* FNV-1 hash each octet of the buffer */
+    while (bp < be) {
+		/* multiply by the 64 bit FNV magic prime mod 2^64 */
+		hval += (hval << 1) + (hval << 4) + (hval << 5) +
+			(hval << 7) + (hval << 8) + (hval << 40);
+		/* xor the bottom with the current octet */
+		hval ^= (uint64_t)*bp++;
+    }
+    return hval;
+}
+
+uint64_t murmur64(const void *key, size_t len) {
+    const uint8_t *data = (const uint8_t *)key;
+    const int nblocks = len / 16;
+
+    uint64_t h1 = 0;
+    uint64_t h2 = 0;
+
+    const uint64_t c1 = 0x87c37b91114253d5ULL;
+    const uint64_t c2 = 0x4cf5ad432745937fULL;
+
+    // Body
+    const uint64_t *blocks = (const uint64_t *)(data);
+    for (int i = 0; i < nblocks; i++) {
+        uint64_t k1 = blocks[i * 2 + 0];
+        uint64_t k2 = blocks[i * 2 + 1];
+
+        k1 *= c1; k1 = (k1 << 31) | (k1 >> (64 - 31)); k1 *= c2; h1 ^= k1;
+        h1 = (h1 << 27) | (h1 >> (64 - 27)); h1 += h2; h1 = h1 * 5 + 0x52dce729;
+
+        k2 *= c2; k2 = (k2 << 33) | (k2 >> (64 - 33)); k2 *= c1; h2 ^= k2;
+        h2 = (h2 << 31) | (h2 >> (64 - 31)); h2 += h1; h2 = h2 * 5 + 0x38495ab5;
+    }
+
+    // Tail
+    const uint8_t *tail = (const uint8_t *)(data + nblocks * 16);
+    uint64_t k1 = 0;
+    uint64_t k2 = 0;
+
+    switch (len & 15) {
+        case 15: k2 ^= ((uint64_t)tail[14]) << 48;
+        case 14: k2 ^= ((uint64_t)tail[13]) << 40;
+        case 13: k2 ^= ((uint64_t)tail[12]) << 32;
+        case 12: k2 ^= ((uint64_t)tail[11]) << 24;
+        case 11: k2 ^= ((uint64_t)tail[10]) << 16;
+        case 10: k2 ^= ((uint64_t)tail[ 9]) << 8;
+        case  9: k2 ^= ((uint64_t)tail[ 8]) << 0;
+                 k2 *= c2; k2 = (k2 << 33) | (k2 >> (64 - 33)); k2 *= c1; h2 ^= k2;
+        case  8: k1 ^= ((uint64_t)tail[ 7]) << 56;
+        case  7: k1 ^= ((uint64_t)tail[ 6]) << 48;
+        case  6: k1 ^= ((uint64_t)tail[ 5]) << 40;
+        case  5: k1 ^= ((uint64_t)tail[ 4]) << 32;
+        case  4: k1 ^= ((uint64_t)tail[ 3]) << 24;
+        case  3: k1 ^= ((uint64_t)tail[ 2]) << 16;
+        case  2: k1 ^= ((uint64_t)tail[ 1]) << 8;
+        case  1: k1 ^= ((uint64_t)tail[ 0]) << 0;
+                 k1 *= c1; k1 = (k1 << 31) | (k1 >> (64 - 31)); k1 *= c2; h1 ^= k1;
+    }
+
+    // Finalization
+    h1 ^= len;
+    h2 ^= len;
+
+    h1 += h2;
+    h2 += h1;
+
+    // fmix64
+    h1 ^= h1 >> 33;
+    h1 *= 0xff51afd7ed558ccdULL;
+    h1 ^= h1 >> 33;
+    h1 *= 0xc4ceb9fe1a85ec53ULL;
+    h1 ^= h1 >> 33;
+
+    return h1;
+}
+
+#endif
